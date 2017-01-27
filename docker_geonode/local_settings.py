1cc3ca3f9fea4402aaeee4a81affea304551aa9e
Seno @ ThinkPad <senoadiw@gmail.com>
2017-01-27 16:31:27 +0700

    fix: KeyError on logout

--- docker_geonode/local_settings.py
+++ docker_geonode/local_settings.py
@@ -107,8 +107,8 @@ LEAFLET_CONFIG = {
 # Legacy from Geoserver
 OGC_SERVER = {
     'default': {
-        'LOGIN_ENDPOINT': 'account_login',
-        'LOGOUT_ENDPOINT': 'account_logout', ### Fixes LOGOUT_ENDPOINT key error on logout
+        'LOGIN_ENDPOINT': '',
+        'LOGOUT_ENDPOINT': '',
         'LOCATION': SITEURL + 'qgis-server/',
         'PUBLIC_LOCATION': SITEURL + 'qgis-server/',
         'LOG_FILE': '/tmp/qgis-server.log'
